0bf935f138f3a76ec17d244812fd94f556205251
f0rt1234 <t-fujimaki@openupitengineer.jp>
2024-04-25 00:21:58 +0900

    guestlogin_add

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
