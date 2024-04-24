76cc4404a7b52c09d2c0cb6d819ba84a88f1af89
f0rt1234 <t-fujimaki@openupitengineer.jp>
2024-04-24 22:55:50 +0900

    devise_pw_delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root :to => 'notes#index'
   resources :notes, only: [:index, :update, :destroy, :create, :new, :edit, :show] 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
